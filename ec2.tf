ba245b7c04bdae4c3987a67c5c0d4dbcc88ff44f
raguram99 <ramcloud88@gmail.com>
2023-03-02 14:04:01 +0530

    New changes

--- ec2.tf
+++ ec2.tf
@@ -1,21 +1,8 @@
 # configured aws provider with proper credentials
+# Configure the AWS Provider
 provider "aws" {
-  region  = "us-east-1"
-  profile = "terraform-user"
+  region = "ap-south-1"
 }
-
-
-# store the terraform state file in s3
-terraform {
-  backend "s3" {
-    bucket  = "aosnote-terraform-state-bucket"
-    key     = "build/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "terraform-user"
-  }
-}
-
-
 # create default vpc if one does not exit
 resource "aws_default_vpc" "default_vpc" {
 
@@ -97,7 +84,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "myec2key"
+  key_name               = "terraform-key"
   user_data              = file("install_techmax.sh")
 
   tags = {
