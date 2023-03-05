792a9729095f5125d46753b5687ecab263c99024
raguram99 <ramcloud88@gmail.com>
2023-03-05 20:40:11 +0530

    Recent changes02

--- CICD/configure-named-profile.sh
+++ CICD/configure-named-profile.sh
@@ -4,9 +4,9 @@
 set -eu
 
 # configure named profile
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile $PROFILE_NAME 
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile $PROFILE_NAME 
-aws configure set region $AWS_REGION --profile $PROFILE_NAME
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set region $AWS_REGION
 
 # verify that profile is configured
 aws configure list --profile $PROFILE_NAME
